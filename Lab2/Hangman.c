046df4b86ce2349d91a9ebc2bd16d4521959b07c
jwr8 <jwr8@uw.edu>
2016-04-28 12:48:05 -0700

    Hangman fixed for unix

--- Lab2/Hangman.c
+++ Lab2/Hangman.c
@@ -27,18 +27,17 @@ int main() {
 	printf(" the guess\nused will be the first character inputted. User Two is alloted %d wrong character", WRONG_GUESSES);
 	printf(" guesses until\nthey lose the game. Wrong character guesses made will be displayed on the bottom line of the LCD.");
 	printf("\n\nPress any key to continue.\n");
-		mygetch();
+		mygetch(); // waits for any user input
 		char word[MAX_STRING_LEN];
 		int ask = 1;
-		while (ask) {
-			
+		while (ask) { // gets input from User One for word to use
 			printf("\nUSER ONE: Please input your word (Max. %d characters).\n", MAX_STRING_LEN);
 			char temp[10000];
 			scanf("%16[0-9a-zA-Z]", temp);
 			int ch;
 			while ((ch=getchar()) != EOF && ch != '\n');
 			int i;
-			for(int i = 0; i < strlen(temp); i = i + 1){
+			for(i = 0; i < strlen(temp); i = i + 1){
 				temp[i] = tolower(temp[i]);
 			}
 			if (strlen(temp) > MAX_STRING_LEN) {
@@ -48,24 +47,25 @@ int main() {
 				ask = 0;
 			}
 		}
-		for (int i = 0; i < 100; i = i + 1) {
-			printf("|||||||||||||||||||||||||||||||||||||||||||||||||||");
-			printf("|||||||||||||||||||||||||||||||||||||||||||||||||||");
-			printf("||||||||||||||||||\n");
+		int i;
+		for (i = 0; i < 100; i = i + 1) { // Shift terminal down 100 lines to hide the chosen word from User Two
+			printf("\n");
 		}
 		// Print strlen(word) "_" characters on LCD first line
 		char current[strlen(word)];
 		memmove(current, word, strlen(word) + 1);
-		for (int i = 0; i < strlen(word); i = i + 1) {
+		for (i = 0; i < strlen(word); i = i + 1) {
 			current[i] = '_';
 		}
 		char wrongGuesses[WRONG_GUESSES * 2];
 		int wrong = 0;
 		int win = 0;
+		for (i = 0; i < strlen(wrongGuesses); i = i + 1) { // Initializes wrongGuesses to be blank
+			wrongGuesses[i] = ' ';
+		}
 		while (wrong < WRONG_GUESSES) {
-			printf("%s\n", current); // Print current to top line of LCD
-			fflush(stdout);
-			printf("%s\n", wrongGuesses); // Print wrongGuesses to bottom line of LCD
+			printf("Word: %s\n", current); // Print current to top line of LCD
+			printf("Wrong Guesses: %s\n", wrongGuesses); // Print wrongGuesses to bottom line of LCD
 			if(wrong == WRONG_GUESSES - 1) {
 				printf("\nWARNING: One more wrong guess will result in a loss.\n\n");
 			}
@@ -77,18 +77,18 @@ int main() {
 			inputChar = tolower(inputChar);
 			int j;
 			int guessed = 0;
-			for (int j = 0; j < strlen(wrongGuesses); j = j + 1) {
+			for (j = 0; j < strlen(wrongGuesses); j = j + 1) {
 				if (wrongGuesses[j] == inputChar) {
 					guessed = 1;
 				}
 			}
-			for (int j = 0; j < strlen(current); j = j + 1){
+			for (j = 0; j < strlen(current); j = j + 1){
 				if (current[j] == inputChar) {
 					guessed = 1;
 				}
 			}
 			int found = 0;
-			for (int j = 0; j < strlen(word); j = j + 1) {
+			for (j = 0; j < strlen(word); j = j + 1) {
 				if (word[j] == inputChar) {
 					current[j] = inputChar;
 					found = 1;
@@ -100,12 +100,12 @@ int main() {
 				wrongGuesses[wrong * 2 + 1] = ' ';
 				wrong = wrong + 1;
 			} else if (guessed) {
-				printf("\nYou have already guessed this character.");
+				printf("\nYou have already guessed this character.\n");
 			} else {
 				printf("\nLetter found!\n");
 			}
 			int foundAll = 1;
-			for (int j = 0; j < strlen(current); j = j + 1) {
+			for (j = 0; j < strlen(current); j = j + 1) {
 				if (current[j] == '_') {
 					foundAll = 0;
 				}
@@ -119,10 +119,10 @@ int main() {
 			printf("\n");
 		}
 		if (win){
-			printf("%s\n\nYOU WIN!!!", current);
+			printf("%s\n\nYOU WIN!!!\n\n", current);
 			// display win message on LCD top line
 		} else {
-			printf("%s\n\nYOU LOSE!!!", current);
+			printf("%s\n\nYOU LOSE!!!\n\n", current);
 			//display lose message on LCD top line
 		}
 }
