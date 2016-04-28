03f8d17e169bcd00451001c33569dcbe6e603f9a
angie269 <ashin26@uw.edu>
2016-04-27 17:44:33 -0700

    mkfifo() --- creating pipes

---
+++ Lab2/pipes.c
@@ -0,0 +1,22 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+void display(char*);
+int main() {
+    char *path = "/tmp/fifo";
+
+    mkfifo(path, 0666);
+    display(path);
+    return 0;
+}
+
+void display(char *path) {
+    int fd;
+    char buf[512];
+    fd = open(path, O_RDWR);
+    read(fd, buf, sizeof(buf));
+    printf("Test \n", buf);
+    close(fd);
+}
