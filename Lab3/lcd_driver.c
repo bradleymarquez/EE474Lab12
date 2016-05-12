222208b93513778001562b8650696faa3c793fef
angie269 <ashin26@uw.edu>
2016-05-11 17:54:58 -0700

    Update lcd_driver.c

--- Lab3/lcd_driver.c
+++ Lab3/lcd_driver.c
@@ -10,18 +10,16 @@
 #define DATA_ 45
 #define LATCH_ 47
 #define CLOCK_ 67
-#define RS0_ 68
-#define RW0_ 44
+#define RS_ 68
+#define RW_ 44
 #define E0_ 26
-#define RS1_
-#define RW1_
-#define E1_
+#define E1_ 60
 
 #define CHAR_PER_LINE 16
-#define NUM_LINES 2
+#define NUM_LINES 3
 
-static int[] RSArr = {RS0_, RS1_};
-static int[] RWArr = {RW0_, RW1_};
+//static int[] RSArr = RS0_;
+//static int[] RWArr = RW0_;
 static int[] EArr = {E0_, E1_};
 
 /********************* FILE OPERATION FUNCTIONS ***************/
@@ -51,11 +49,11 @@ static int __init driver_entry(void) {
 		printk(KERN_ALERT "lcd_driver: unable to add cdev to kernerl\n");
 		return ret;
 	}
-	
+
 	// Initialize SEMAPHORE
 	sema_init(&virtual_device.sem, 1);
 	msleep(10);
-	
+
 	return 0;
 }
 
@@ -81,24 +79,24 @@ int device_open(struct inode *inode, struct file* filp) {
 	gpio_request(DATA_, "Data");
 	gpio_request(LATCH_, "Latch");
 	gpio_request(CLOCK_, "Clock");
-	gpio_request(RS0_, "RS1");
-	gpio_request(RW0_, "R/W1");
+	gpio_request(RS_, "RS1");
+	gpio_request(RW_, "R/W1");
 	gpio_request(E0_, "E1");
-	gpio_request(RS1_, "RS2");
-	gpio_request(RW1_, "R/W2");
+	//gpio_request(RS1_, "RS2");
+	//gpio_request(RW1_, "R/W2");
 	gpio_request(E1_, "E2");
 
 	// Set all pins for output
 	gpio_direction_output(DATA_, 0);
 	gpio_direction_output(LATCH_, 0);
 	gpio_direction_output(CLOCK_, 0);
-	gpio_direction_output(RS0_, 0);
-	gpio_direction_output(RW0_, 0);
+	gpio_direction_output(RS_, 0);
+	gpio_direction_output(RW_, 0);
 	gpio_direction_output(E0_, 0);
-	gpio_direction_output(RS1_, 0);
-	gpio_direction_output(RW1_, 0);
+	//gpio_direction_output(RS1_, 0);
+	//gpio_direction_output(RW1_, 0);
 	gpio_direction_output(E1_, 0);
-	
+
 	initialize(0);
 	initialize(1);
 
@@ -109,18 +107,18 @@ int device_open(struct inode *inode, struct file* filp) {
 // closes device, clear display, free the GPIO pins, and returns access to semaphore.
 int device_close(struct inode* inode, struct  file *filp) {
 	up(&virtual_device.sem);
-	clearDisplay(0);	
+	clearDisplay(0);
 	displayOff(0);
-	clearDisplay(1);	
+	clearDisplay(1);
 	displayOff(1);
 	gpio_free(DATA_);
 	gpio_free(LATCH_);
 	gpio_free(CLOCK_);
-	gpio_free(RS0_);
-	gpio_free(RW0_);
+	gpio_free(RS_);
+	gpio_free(RW_);
 	gpio_free(E0_);
-	gpio_free(RS1_);
-	gpio_free(RW1_);
+	//gpio_free(RS1_);
+	//gpio_free(RW1_);
 	gpio_free(E1_);
 	return 0;
 }
@@ -133,15 +131,17 @@ ssize_t device_read(struct file* filp, char* bufStoreData, size_t bufCount, loff
 // Called when user wants to send info to device
 // Calling a shift register file
 ssize_t device_write(struct file* filp, const char* bufSource, size_t bufCount, loff_t* curOffset) {
-	int firstLine, secondLine, valid = 1;
+	int firstLine, secondLine, thirdLine, valid = 1;
 
-	// Determine how many lines of the diplay will be used
+	// Determine how many lines of the display will be used
 	if (bufCount > (CHAR_PER_LINE * NUM_LINES) + 1) {
 		firstLine = CHAR_PER_LINE;
 		secondLine = CHAR_PER_LINE;
+		thirdLine = CHAR_PER_LINE; //
 	} else if (bufCount > CHAR_PER_LINE) {
 		firstLine = CHAR_PER_LINE;
 		secondLine = bufCount - CHAR_PER_LINE;
+		thirdLine = bufCount - (2*CHAR_PER_LINE);
 	} else if (bufCount > 1) {
 		firstLine = bufCount;
 		secondLine = 0;
@@ -164,26 +164,30 @@ ssize_t device_write(struct file* filp, const char* bufSource, size_t bufCount, 
 			writeChar(bufSource[i + CHAR_PER_LINE], screenSel);
 		}
 
+		for (i = 1; i <= thirdLine; i++) {
+			writeChar(bufSource[i + (2*CHAR_PER_LINE)], screenSel);
+		}
+
 	}
 	return copy_from_user(virtual_device.data, bufSource, bufCount);
 }
 
 // Initializes the LCD with the proper series of commands
 void initialize(int screenSel) {
-	gpio_set_value(RSArr[screenSel], 0);
-	gpio_set_value(RWArr[screenSel], 0);
+	gpio_set_value(RS_, 0);
+	gpio_set_value(RW_, 0);
 
 	msleep(15);
-	
+
 	command((unsigned char) 0x30, screenSel); // Function Set #1
 	msleep(5);
-	
+
 	lcdSend(screenSel); // Function Set #2
 	msleep(1);
 
 	lcdSend(screenSel); // Function Set #3
 	msleep(1);
-	
+
 	if (screenSel) { // Function Set #4
 		command((unsigned char) 0x38, screenSel); // 5x7 font, 2 lines
 	} else {
@@ -196,14 +200,14 @@ void initialize(int screenSel) {
 
 	command((unsigned char) 0x01, screenSel); // Clear Display
 	msleep(16);
-	
+
 	if (screenSel) { // Entry Mode Set
 		command((unsigned char) 0x06, screenSel); // Increment mode
 	} else {
 		command((unsigned char) 0x05, screenSel); // Decrement mode
 	}
 	udelay(50);
-	
+
 	if (screenSel) { // Entry Mode Set
 		command((unsigned char) 0x0C, screenSel); // Cursor OFF, Blink OFF
 	} else {
@@ -237,12 +241,12 @@ void setBus(unsigned char num) {
 		gpio_set_value(DATA_, binary[i]);  // Set the data line to the next value
 
 		// Toggle the clock
-		gpio_set_value(CLOCK_, 1);		
+		gpio_set_value(CLOCK_, 1);
 		udelay(10);
 		gpio_set_value(CLOCK_, 0);
 		i--;
 	}
-	
+
 	// Toggle the latch
 	gpio_set_value(LATCH_, 1);
 	udelay(50);
@@ -252,24 +256,24 @@ void setBus(unsigned char num) {
 
 // Clears the LCD
 void clearDisplay(int screenSel){
-	gpio_set_value(RSArr[screenSel], 0);
-	gpio_set_value(RWArr[screenSel], 0);
+	gpio_set_value(RS_, 0);
+	gpio_set_value(RW_, 0);
 	command ((unsigned char) 0x01, screenSel); // Clear Display
 	msleep(16);
 }
 
 // Turns the LCD off
 void displayOff(int screenSel) {
-	gpio_set_value(RSArr[screenSel], 0);
-	gpio_set_value(RWArr[screenSel], 0);
+	gpio_set_value(RS_, 0);
+	gpio_set_value(RW_, 0);
 	command((unsigned char) 0x08, screenSel); // Display OFF
 	udelay(50);
 }
 
 // Sets the R/W pointer to the address specified
 void setAddress(unsigned char address, int screenSel) {
-	gpio_set_value(RSArr[screenSel], 0);
-	gpio_set_value(RWArr[screenSel], 0);
+	gpio_set_value(RS_, 0);
+	gpio_set_value(RW_, 0);
 	address |= 0x80;
 	setBus(address);
 	lcdSend(screenSel);
@@ -278,8 +282,8 @@ void setAddress(unsigned char address, int screenSel) {
 
 // Sets DB7 to DB0 to the given 8 bits
 void writeChar(unsigned char character, int screenSel) {
-	gpio_set_value(RSArr[screenSel], 1);
-	gpio_set_value(RWArr[screenSel], 0);
+	gpio_set_value(RS_, 1);
+	gpio_set_value(RW_, 0);
 	setBus(character);
 	lcdSend(screenSel);
 	udelay(50);
